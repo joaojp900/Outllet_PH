1824650273e1b62eebe35fe74138e994fc91461c
edgar34c <edgararaujocorrea@gmail.com>
2023-04-14 16:57:41 -0300

    Carrinho com bug

--- view/carrinho.php
+++ view/carrinho.php
@@ -17,13 +17,15 @@
     <div class="logo">
         <img src="image/header.jpeg" alt="Logo">
     </div>
-
+    <?php
+        
+    ?>
         <!--Icones whatsapp e instagram-->
 	<div class="whatsapp">
         <a href="https://api.whatsapp.com/send/?phone=5511940547458&text&type=phone_number&app_absent=0" target="_blank"><img src="https://storage.googleapis.com/neuro-cdn/uploads/72d189bd35267b7a5707699a3705e293.png" alt="whatsapp"></a>
         <a href="https://www.instagram.com/outllet_ph/" target="_blank"><img src="image/instagram.png" alt="Instagram" ></a>
     </div>
-    
+
 
 </body>
 </html>
