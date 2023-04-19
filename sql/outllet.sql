61d11c37753db74b9d061d87e37ad2bac6c5cf2d
edgar34c <edgararaujocorrea@gmail.com>
2023-04-19 18:40:08 -0300

    BD

---
+++ sql/outllet.sql
@@ -0,0 +1,102 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 19-Abr-2023 às 23:38
+-- Versão do servidor: 10.4.27-MariaDB
+-- versão do PHP: 8.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `outllet`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `produtos`
+--
+
+CREATE TABLE `produtos` (
+  `codproduto` int(11) NOT NULL,
+  `nome` varchar(250) NOT NULL,
+  `preco` int(11) NOT NULL,
+  `descricao` varchar(250) NOT NULL,
+  `imagem` varchar(220) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Extraindo dados da tabela `produtos`
+--
+
+INSERT INTO `produtos` (`codproduto`, `nome`, `preco`, `descricao`, `imagem`) VALUES
+(61, 'MIZUNO PRO 3', 340, 'MIZUNO MAIS RELÍQUIA DE TODOS E importado qualidade viatnã', '1f9db6a0be427c8b9c3550e0dc86a38a.jpeg'),
+(62, 'MIZUNOS PRO 6,7,8,9,X,11', 300, 'MIZUNO IMPORTADOS \r\n\r\nCONSULTAR TAMANHOS DISPONÍVEIS\r\n\r\nVIATNÃ\r\nCONFORTO 100%\r\nTÊNIS ESPORTIVO', 'c56e21505b576cbbc7049bbdcd4c8d4e.jpeg'),
+(66, 'Nike dunk', 450, 'Nike dunk', '7f8f35a2cbcff96bb27246a9170a0b3f.jpeg');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `usuario`
+--
+
+CREATE TABLE `usuario` (
+  `id` int(255) NOT NULL,
+  `nome` varchar(255) NOT NULL,
+  `senha` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Extraindo dados da tabela `usuario`
+--
+
+INSERT INTO `usuario` (`id`, `nome`, `senha`) VALUES
+(1, 'ed', '123'),
+(3, 'ph', '123');
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices para tabela `produtos`
+--
+ALTER TABLE `produtos`
+  ADD PRIMARY KEY (`codproduto`);
+
+--
+-- Índices para tabela `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `produtos`
+--
+ALTER TABLE `produtos`
+  MODIFY `codproduto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=68;
+
+--
+-- AUTO_INCREMENT de tabela `usuario`
+--
+ALTER TABLE `usuario`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
