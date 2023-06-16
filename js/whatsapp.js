a8d23a166dbc8919d888f1f9187117abde6c1800
edgar34c <edgararaujocorrea@gmail.com>
2023-06-16 12:46:22 -0300

    Form do whats funcionando

--- js/whatsapp.js
+++ js/whatsapp.js
@@ -14,11 +14,11 @@ function whatsappSend(){
         }
         return response.json()
     }).then(data => {
-        console.log(data)
-        console.log(data.codproduto)
-        /*var msg = `${msg_bnt}%0aMeus%20dados:%0aNome:${nome.value}%0aCPF:${cpf.value}%0aWhatsapp:${tel.value}%0aEndereço:${endereco.value}`
+        var valorTotal = data[1].quantidade * data[1].preco
+        var valorTotal = valorTotal.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+        var msg = `${msg_bnt}%0aMeus%20dados:%0aNome:${nome.value}%0aCPF:${cpf.value}%0aWhatsapp:${tel.value}%0aEndereço:${endereco.value}%0aProduto:${data[1].nome}%0aQuantidade:${data[1].quantidade}%0aValor total:${valorTotal}`
         var url = `https://api.whatsapp.com/send/?phone=${telDest}&text=${msg}&type=phone_number&app_absent=0`;
-        window.open(url, '_blank').focus();*/
+        window.open(url, '_blank').focus();
     }).catch(error => {
         console.error('Ocorreu um erro', error)
     })
